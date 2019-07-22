b53baf11cbfa02d11fc917ae20d7d95dc0632d01
velvetops <quantumular@gmail.com>
2019-07-23 03:19:07 +0900

    ray

---
+++ src/ray.rs
@@ -0,0 +1,29 @@
+use crate::vec3::Vec3;
+
+#[derive(Debug, Clone, Copy)]
+pub struct Ray {
+    pub origin: Vec3,
+    pub direction: Vec3,
+}
+
+impl Ray {
+    pub fn point_at_parameter(&self, t: f32) -> Vec3 {
+        self.origin + self.direction * t
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::vec3::Vec3;
+
+    #[test]
+    fn test_point_at_parameter() {
+        let ray = Ray {
+            origin: Vec3::new(0.0, 0.0, 0.0),
+            direction: Vec3::new(1.0, 2.0, 3.0),
+        };
+        let t: f32 = 2.0;
+        assert_eq!(ray.point_at_parameter(t), Vec3::new(2.0, 4.0, 6.0));
+    }
+}
