0b4d9f27801182f979d7131e6b8c4998c3c8172c
velvetops <quantumular@gmail.com>
2019-07-23 02:17:34 +0900

    add unit test of Vec3

--- src/vec3.rs
+++ src/vec3.rs
@@ -1,6 +1,6 @@
 use std::ops::{Add, Div, Mul, Neg, Sub};
 
-#[derive(Debug, Copy, Clone)]
+#[derive(Debug, PartialEq, Copy, Clone)]
 pub struct Vec3 {
     pub x: f32,
     pub y: f32,
@@ -49,7 +49,7 @@ impl Vec3 {
 impl Add for Vec3 {
     type Output = Vec3;
 
-    fn add(self, other: Vec3) -> Vec3 {
+    fn add(self, other: Vec3) -> Self {
         Vec3 {
             x: self.x + other.x,
             y: self.y + other.y,
@@ -61,7 +61,7 @@ impl Add for Vec3 {
 impl Sub for Vec3 {
     type Output = Vec3;
 
-    fn sub(self, other: Vec3) -> Vec3 {
+    fn sub(self, other: Vec3) -> Self {
         Vec3 {
             x: self.x - other.x,
             y: self.y - other.y,
@@ -73,7 +73,7 @@ impl Sub for Vec3 {
 impl Mul<f32> for Vec3 {
     type Output = Vec3;
 
-    fn mul(self, coef: f32) -> Vec3 {
+    fn mul(self, coef: f32) -> Self {
         Vec3 {
             x: self.x * coef,
             y: self.y * coef,
@@ -85,7 +85,7 @@ impl Mul<f32> for Vec3 {
 impl Div<f32> for Vec3 {
     type Output = Vec3;
 
-    fn div(self, coef: f32) -> Vec3 {
+    fn div(self, coef: f32) -> Self {
         Vec3 {
             x: self.x / coef,
             y: self.y / coef,
@@ -97,11 +97,104 @@ impl Div<f32> for Vec3 {
 impl Neg for Vec3 {
     type Output = Vec3;
 
-    fn neg(self) -> Vec3 {
+    fn neg(self) -> Self {
         Vec3 {
             x: -self.x,
             y: -self.y,
             z: -self.z,
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn get_new_vec() {
+        let v1 = Vec3::new(1.0, 2.0, 3.0);
+        let v2 = Vec3 {
+            x: 1.0,
+            y: 2.0,
+            z: 3.0,
+        };
+        assert_eq!(v1, v2);
+    }
+
+    #[test]
+    fn get_vec_length() {
+        let v = Vec3::new(0.0, 3.0, 4.0);
+        assert_eq!(v.length(), 5.0);
+    }
+
+    #[test]
+    fn get_veg_squared_length() {
+        let v = Vec3::new(1.0, 2.0, 3.0);
+        assert_eq!(v.squared_length(), 14.0);
+    }
+
+    #[test]
+    fn get_unit_vector() {
+        let v1 = Vec3 {
+            x: 1.0,
+            y: 1.0,
+            z: 1.0,
+        };
+        let v2 = Vec3::make_unit_vector();
+        assert_eq!(v1, v2);
+    }
+
+    #[test]
+    fn get_inner_product() {
+        let v1 = Vec3::new(1.0, 2.0, 3.0);
+        let v2 = Vec3::new(4.0, 5.0, 6.0);
+        assert_eq!(v1.dot(&v2), 32.0);
+    }
+
+    #[test]
+    fn get_cross_product() {
+        let v1 = Vec3::new(1.0, 2.0, 3.0);
+        let v2 = Vec3::new(4.0, 5.0, 6.0);
+        let v_expected = Vec3::new(-3.0, 6.0, -3.0);
+        assert_eq!(v1.cross(&v2), v_expected);
+    }
+
+    #[test]
+    fn v_add() {
+        let v1 = Vec3::new(1.0, 2.0, 3.0);
+        let v2 = Vec3::new(4.0, 5.0, 6.0);
+        let v_expected = Vec3::new(5.0, 7.0, 9.0);
+        assert_eq!(v1 + v2, v_expected);
+    }
+
+    #[test]
+    fn v_sub() {
+        let v1 = Vec3::new(1.0, 2.0, 3.0);
+        let v2 = Vec3::new(4.0, 5.0, 6.0);
+        let v_expected = Vec3::new(-3.0, -3.0, -3.0);
+        assert_eq!(v1 - v2, v_expected);
+    }
+
+    #[test]
+    fn v_mul() {
+        let v = Vec3::new(1.0, 2.0, 3.0);
+        let a: f32 = 2.0;
+        let v_expected = Vec3::new(2.0, 4.0, 6.0);
+        assert_eq!(v * a, v_expected);
+    }
+
+    #[test]
+    fn v_div() {
+        let v = Vec3::new(3.0, 6.0, 9.0);
+        let a: f32 = 3.0;
+        let v_expected = Vec3::new(1.0, 2.0, 3.0);
+        assert_eq!(v / a, v_expected);
+    }
+
+    #[test]
+    fn v_neg() {
+        let v1 = Vec3::new(1.0, 2.0, 3.0);
+        let v_expected = Vec3::new(-1.0, -2.0, -3.0);
+        assert_eq!(-v1, v_expected);
     }
 }
