d8b863d3adaea6a2af104438c9310ac87978556d
velvetops <quantumular@gmail.com>
2019-07-22 16:12:23 +0900

    output ppm

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use std::fs;
+use std::io::{BufWriter, Write};
+
 fn main() {
-    println!("Hello, world!");
+    let nx: u32 = 200;
+    let ny: u32 = 100;
+
+    let mut f = BufWriter::new(fs::File::create("result.ppm").unwrap());
+    f.write_all(format!("P3\n{} {}\n255\n", nx, ny).as_bytes())
+        .unwrap();
+
+    for j in (0..ny).rev() {
+        for i in 0..nx {
+            let r = i as f32 / nx as f32;
+            let g = j as f32 / ny as f32;
+            let b = 0.2f32;
+
+            let ir = (255.99 * r) as i32;
+            let ig = (255.99 * g) as i32;
+            let ib = (255.99 * b) as i32;
+
+            f.write(format!("{} {} {}\n", ir, ig, ib).as_bytes())
+                .unwrap();
+        }
+    }
 }
