af53debf96169e92bdf075c19e906fc5a67d5f4e
velvetops <quantumular@gmail.com>
2019-07-26 17:29:12 +0900

    implemented parallel processing

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use rand;
 use rayon::prelude::*;
 use std::fs;
 use std::io::{BufWriter, Write};
+use std::time::Instant;
 
 mod camera;
 mod hitable;
@@ -42,6 +43,7 @@ fn main() {
     let nx: u32 = 640;
     let ny: u32 = 480;
     let ns: u32 = 40; // number of samples inside each pixel
+
     // Objects setup
     let world = scene::random_scene();
 
@@ -55,8 +57,11 @@ fn main() {
     let dist_to_focus: f32 = (lookfrom - lookat).length();
     let cam = Camera::new(lookfrom, lookat, vup, vfov, aspect, aperture, dist_to_focus);
 
-    // Render
-    let mut f = BufWriter::new(fs::File::create("image/parallel/").unwrap());
+    // Sequential process
+
+    let start = Instant::now();
+
+    let mut f = BufWriter::new(fs::File::create("./image/comparison/sequential.ppm").unwrap());
     f.write_all(format!("P3\n{} {}\n255\n", nx, ny).as_bytes())
         .unwrap();
 
@@ -80,4 +85,55 @@ fn main() {
                 .unwrap();
         }
     }
+
+    let duration = start.elapsed();
+    println!("Time elapsed in sequential process: {:?}", duration);
+
+    // Parallell process
+    let start = Instant::now();
+
+    let mut f = BufWriter::new(fs::File::create("image/comparison/parallel.ppm").unwrap());
+    f.write_all(format!("P3\n{} {}\n255\n", nx, ny).as_bytes())
+        .unwrap();
+
+    let par_vec: Vec<(u32, u32)> = {
+        let mut v: Vec<(u32, u32)> = vec![];
+
+        for j in (0..ny).rev() {
+            for i in 0..nx {
+                v.push((i, j));
+            }
+        }
+        v
+    };
+
+    let pixels: Vec<Vec3> = par_vec
+        .par_iter()
+        .cloned()
+        .map(|(i, j)| {
+            let mut col: Vec3 = (0..ns)
+                .into_par_iter()
+                .map(|_| {
+                    let u = (i as f32 + rand::random::<f32>()) / nx as f32;
+                    let v = (j as f32 + rand::random::<f32>()) / ny as f32;
+                    let r: Ray = cam.get_ray(u, v);
+                    color(&r, &world, 0)
+                })
+                .reduce(|| Vec3::new(0.0, 0.0, 0.0), |sum, x| sum + x);
+            col /= ns as f32;
+            col = Vec3::new(col.x.sqrt(), col.y.sqrt(), col.z.sqrt());
+            col
+        })
+        .collect();
+
+    for pix in pixels {
+        let ir = (255.99 * pix.x) as i32;
+        let ig = (255.99 * pix.y) as i32;
+        let ib = (255.99 * pix.z) as i32;
+        f.write(format!("{} {} {}\n", ir, ig, ib).as_bytes())
+            .unwrap();
+    }
+
+    let duration = start.elapsed();
+    println!("Time elapsed in parallel process  : {:?}", duration);
 }
